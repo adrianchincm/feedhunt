a49b466346dd5e77d975d542c30048f2bbabad5b
Adrian Chin <syncing95@gmail.com>
2020-12-29 15:24:38 +0800

    added date functionality to post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import avatarTest from '../images/img_avatar.png';
+import { timeSince } from '../shared/utility'
 
 const Post = props => {
-
+    
     return (
         <div class="flex flex-row justify-left p-2 cursor-pointer items-center
         transition duration-300 ease-in-out hover:bg-bgPrimaryLight 
@@ -11,8 +12,14 @@ const Post = props => {
                     alt="avatar"
                     class="rounded-full w-50px h-50px "
                     />
-            <div class="flex flex-col ml-4 text-left">               
-                <p class="font-bold mb-0">{props.post.owner.username}</p>
+            <div class="flex flex-col ml-4 text-left">
+                <div class="flex"> 
+                    <p class="font-bold mb-0">{props.post.owner.username}</p>
+                    <p class="ml-2 mb-0 text-textgray">{props.post.owner.username}</p>
+                    <span class="ml-2 text-textgray">|</span>
+                    <p class="ml-2 mb-0 text-textgray">{timeSince(new Date(props.post.createdAt))}</p>
+                </div>               
+                
                 <p class="">{props.post.content}</p>                
             </div>
         </div>
--- src/shared/utility.js
+++ src/shared/utility.js
@@ -4,3 +4,35 @@ export const updateObject = (oldObject, updatedProperties) => {
         ...updatedProperties
     };
 }
+
+export function timeSince(date) {
+    const dateNum = date.getDate();
+    const month = date.toLocaleDateString("en-US", { month: "short" });
+    const year = date.toLocaleDateString("en-US", { year: "numeric" });
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+  
+    var interval = seconds / 31536000;
+  
+    if (interval > 1) {
+        return `${dateNum} ${month} ${year}`
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+        return `${dateNum} ${month}`
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+        return `${dateNum} ${month}`
+    }
+
+    interval = seconds / 3600;
+    if (interval > 1) {
+        return `${Math.floor(interval)}h`
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+        return `${Math.floor(interval)}m`
+    }
+    return Math.floor(seconds) + "s";
+  }
